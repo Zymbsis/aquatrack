adae2b2fff2493f2ec16227a921b669d9d44ce66
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-08-01 14:26:35 +0200

    init

--- src/shared/components/Logo/Logo.jsx
+++ src/shared/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 const Logo = ({ className }) => {
   return (
     <a
-      href="/project-digitall3.0-r/"
+      href="/aquatrack"
       className={clsx(css.logo, { [className]: className }, 'tour-logo')}
     >
       AquaTrack
