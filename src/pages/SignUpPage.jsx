110a83fac1f33f998238d4f3a47f627d0ca8c5e3
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2024-08-31 14:34:07 +0200

    fix deploy

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,13 +1,9 @@
 import { Container, Logo, Section } from 'shared';
 import { AdvantagesSection, SignUpSection } from 'components';
 import { useWindowSize } from 'helpers';
-import { useSelector } from 'react-redux';
-import { selectIsSendMail } from '../redux/auth/selectors';
 
 const SignUpPage = () => {
   const windowSize = useWindowSize();
-  const isSendMail = useSelector(selectIsSendMail);
-  console.log(isSendMail);
   return (
     <Section>
       <Container>
